c59012ab83e3b241ffc55b1d219715e7762937cd
Sam Ritchie <sritchie@twitter.com>
2011-09-05 00:05:27 -0700

    bumped dependencies and project version.

--- project.clj
+++ project.clj
@@ -1,19 +1,12 @@
-(defproject elephantdb/elephantdb-cascading "0.0.4"
-  :source-path "src/clj"
-  :test-path "test/clj"
+(defproject elephantdb/elephantdb-cascading "0.0.7"
   :java-source-path "src/jvm"
   :java-fork "true"
   :javac-debug "true"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [elephantdb "0.0.4"]
-                 [cascading/cascading-core "1.2-wip-63" :exclusions [org.codehaus.janino/janino]]
-                 ]
-  :repositories {
-                 "conjars" "http://conjars.org/repo"
-                 }
-  :dev-dependencies [
-                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [swank-clojure "1.2.1"]
-                    ]
-              )
+                 [elephantdb "0.0.7"]
+                 [cascading/cascading-core "1.2.4"
+                  :exclusions [org.codehaus.janino/janino]]]
+  :repositories {"conjars" "http://conjars.org/repo"}
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                     [swank-clojure "1.2.1"]])
