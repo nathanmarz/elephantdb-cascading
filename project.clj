eece2ab643c664e5c144c4ea9d414311b2731d53
Sam Ritchie <sritchie09@gmail.com>
2011-12-15 18:13:59 -0800

    tweaked dependencies in project.clj

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject elephantdb/elephantdb-cascading "0.2.0"
+(defproject elephantdb/elephantdb-cascading "0.3.0"
+  :source-path "src/clj"
   :java-source-path "src/jvm"
   :test-path "test/clj"
-  :java-fork "true"
-  :javac-debug "true"
+  :javac-options {:debug "true" :fork "true"}
+  :repositories {"conjars" "http://conjars.org/repo"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [elephantdb "0.2.0"]
-                 [cascading/cascading-core "1.2.4"
-                  :exclusions [org.codehaus.janino/janino]]]
-  :repositories {"conjars" "http://conjars.org/repo"}
-  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [swank-clojure "1.4.0-SNAPSHOT"]])
+                 [cascading/cascading-hadoop "2.0.0-wip-166"
+                  :exclusions [org.codehaus.janino/janino
+                               org.apache.hadoop/hadoop-core]]]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]])
