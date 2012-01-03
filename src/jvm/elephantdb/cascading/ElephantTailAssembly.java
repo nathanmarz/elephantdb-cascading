9cda3012ed4fcbb70d20673bcf790111e93b60af
Sam Ritchie <sritchie09@gmail.com>
2012-01-03 09:37:17 -0800

    new interfaces in tail.

--- src/jvm/elephantdb/cascading/ElephantTailAssembly.java
+++ src/jvm/elephantdb/cascading/ElephantTailAssembly.java
@@ -12,7 +12,9 @@ import cascading.pipe.SubAssembly;
 import cascading.tuple.Fields;
 import cascading.tuple.Tuple;
 import elephantdb.DomainSpec;
-import elephantdb.persistence.KryoWrapper;
+import elephantdb.Utils;
+import elephantdb.partition.ShardingScheme;
+import elephantdb.serialize.Serializer;
 import org.apache.hadoop.io.BytesWritable;
 import org.apache.log4j.Logger;
 
@@ -22,37 +24,38 @@ public class ElephantTailAssembly extends SubAssembly {
     public static Logger LOG = Logger.getLogger(ElephantTailAssembly.class);
 
     public static class Shardize extends BaseOperation implements Function {
-        DomainSpec _spec;
+        ShardingScheme shardScheme;
+        int shardCount;
 
         public Shardize(String outfield, DomainSpec spec) {
             super(new Fields(outfield));
-            _spec = spec;
+            shardScheme = spec.getShardScheme();
+            shardCount = spec.getNumShards();
+        }
+
+        public int shardIndex(Object key) {
+            return shardScheme.shardIndex(key, shardCount);
         }
 
         public void operate(FlowProcess process, FunctionCall call) {
-            LOG.info("Shard Args are: " + call.getArguments());
-
             Object key = call.getArguments().getObject(0);
 
-            LOG.info("key is " + key);
-            int shard = _spec.shardIndex(key);
+            int shard = shardIndex(key);
             call.getOutputCollector().add(new Tuple(shard));
         }
     }
 
     public static class MakeSortableKey extends BaseOperation implements Function {
-        DomainSpec _spec;
-        KryoWrapper.KryoBuffer _kryoBuf;
+        Serializer serializer;
 
         public MakeSortableKey(String outfield, DomainSpec spec) {
             super(new Fields(outfield));
-            _spec = spec;
-            _kryoBuf = _spec.getCoordinator().getKryoBuffer();
+            serializer = Utils.makeSerializer(spec);
         }
 
         public void operate(FlowProcess process, FunctionCall call) {
             Object key = call.getArguments().getObject(0);
-            BytesWritable sortField = new BytesWritable(_kryoBuf.serialize(key));
+            BytesWritable sortField = new BytesWritable(serializer.serialize(key));
             call.getOutputCollector().add(new Tuple(sortField));
         }
     }
@@ -66,7 +69,8 @@ public class ElephantTailAssembly extends SubAssembly {
         LOG.info("Instantiating spec: " + spec);
 
         // Add the shard index as field #2.
-        Pipe out = new Each(keyValuePairs, new Fields(0), new Shardize(shardField, spec), Fields.ALL);
+        Pipe out =
+            new Each(keyValuePairs, new Fields(0), new Shardize(shardField, spec), Fields.ALL);
 
         // Add the serialized key itself as field #3 for sorting.
         // TODO: Make secondary sorting optional, and come up with a function to generate
