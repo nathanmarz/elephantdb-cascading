16ba76fbf4470dc147efc10b610d332b4e05e8c6
Sam Ritchie <sritchie09@gmail.com>
2012-01-03 09:31:54 -0800

    moved tests.

--- project.clj
+++ project.clj
@@ -1,12 +1,9 @@
 (defproject elephantdb/elephantdb-cascading "0.2.0"
   :java-source-path "src/jvm"
-  :test-path "test/clj"
-  :java-fork "true"
-  :javac-debug "true"
+  :javac-options {:debug "true" :fork "true"}
+  :repositories {"conjars" "http://conjars.org/repo"}
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [elephantdb "0.2.0"]
                  [cascading/cascading-core "1.2.4"
                   :exclusions [org.codehaus.janino/janino]]]
-  :repositories {"conjars" "http://conjars.org/repo"}
-  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [swank-clojure "1.4.0-SNAPSHOT"]])
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]])
