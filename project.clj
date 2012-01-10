44effb492dab1f6c4b03a5a4ced1b68983199a90
Sam Ritchie <sritchie09@gmail.com>
2012-01-10 11:31:04 -0500

    added explicit log4j

--- project.clj
+++ project.clj
@@ -5,9 +5,10 @@
   :repositories {"conjars" "http://conjars.org/repo"}
   :dependencies [[elephantdb "0.2.0"]
                  [hadoop-util "0.2.7"]
+                 [log4j/log4j "1.2.16"]
                  [cascading/cascading-hadoop "2.0.0-wip-184"
                   :exclusions [org.codehaus.janino/janino
                                org.apache.hadoop/hadoop-core]]]
-  :dev-dependencies [[org.clojure/clojure "1.3.0"]
-                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                     [org.clojure/clojure "1.3.0"]
                      [midje "1.3.0"]])
