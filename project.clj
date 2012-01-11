a3d64d05614bce504ba7a4c5e57462d9efdc4a3e
Sam Ritchie <sritchie09@gmail.com>
2012-01-10 19:55:43 -0500

    fixed log dependency.

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
   :repositories {"conjars" "http://conjars.org/repo"}
   :dependencies [[elephantdb "0.2.0"]
                  [hadoop-util "0.2.7"]
-                 [log4j/log4j "1.2.16"]
+                 [org.slf4j/slf4j-api "1.6.1"]
                  [cascading/cascading-hadoop "2.0.0-wip-184"
                   :exclusions [org.codehaus.janino/janino
                                org.apache.hadoop/hadoop-core]]]
