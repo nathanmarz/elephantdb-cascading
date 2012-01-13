6a74050cc200ec4d3e03f0f8b18a98a8d05e7a56
Sam Ritchie <sritchie09@gmail.com>
2012-01-13 10:06:27 -0400

    Beefed up initial integration test.

--- test/elephantdb/cascading/integration_test.clj
+++ test/elephantdb/cascading/integration_test.clj
@@ -1,9 +1,9 @@
 (ns elephantdb.cascading.integration-test
   (:use midje.sweet
-        elephantdb.test.common
-        [clojure.string :only (join)])
+        elephantdb.test.common)
   (:require [elephantdb.test.keyval :as t]
-            [hadoop-util.test :as test])
+            [hadoop-util.test :as test]
+            [clojure.string :as s])
   (:import [cascading.pipe Pipe]
            [cascading.tuple Fields Tuple]
            [cascading.flow FlowConnector]
@@ -20,6 +20,14 @@
            [org.apache.hadoop.mapred JobConf]))
 
 ;; ## Key-Value
+
+(defn kv-spec
+  "Returns a DomainSpec initialized with a hash-mod sharding scheme, a
+  key-value persistence and the supplied sharding count."
+  [shard-count]
+  (DomainSpec. (JavaBerkDB.)
+               (HashModScheme.)
+               shard-count))
 
 (defn hfs-tap
   "Returns an Hfs tap with the default sequencefile scheme and the
@@ -46,29 +54,38 @@
     ret))
 
 (def defaults
-  ["org.apache.hadoop.io.serializer.WritableSerialization"
-   "cascading.tuple.hadoop.BytesSerialization"
-   "cascading.tuple.hadoop.TupleSerialization"])
+  {"io.serializations"
+   (s/join "," ["org.apache.hadoop.io.serializer.WritableSerialization"
+                "cascading.tuple.hadoop.BytesSerialization"
+                "cascading.tuple.hadoop.TupleSerialization"])})
+
+(def mk-props
+  (partial merge defaults))
+
+(defn job-conf
+  "Returns a JobConf instance, optionally augmented by the supplied
+   property map."
+  ([] (job-conf {}))
+  ([prop-map]
+     (let [conf (JobConf.)]
+       (doseq [[k v] (mk-props prop-map)]
+         (.set conf k v))
+       conf)))
+
+(defn flow-connector
+  "Returns an instance of FlowConnection, optionally augmented by the
+   supplied property map."
+  ([] (flow-connector {}))
+  ([prop-map]
+     (FlowConnector. (mk-props prop-map))))
 
 (defn conj-serialization!
   "Appends the supplied serialization to the supplied JobConf
   object. Returns the modified JobConf object."
   [conf serialization]
-  (.set conf
-        "io.serializations"
-        (join "," (conj defaults serialization))))
-
-(def default-props)
-
-(defn jobconf
-  "Returns a JobConf instance, optionally augmented by the supplied
-   property map.."
-  ([] (jobconf {}))
-  ([prop-map]
-     (let [conf (JobConf.)]
-       (doseq [[k v] prop-map]
-         (.set conf k v))
-       conf)))
+  (let [old-val (.get conf "io.serializations")
+        new-val (str old-val "," serialization)]
+    (.set conf "io.serializations" new-val)))
 
 (defn populate!
   "Accepts a key-value tap, a sequence of key-value pairs (and an
@@ -76,33 +93,43 @@
   and sinks all key-value pairs into the tap. Returns the original tap
   instance.."
   [kv-tap kv-pairs & {:keys [props]}]
-  (with-open [collector (.openForWrite kv-tap (jobconf props))]
+  (with-open [collector (.openForWrite kv-tap (job-conf props))]
     (doseq [[k v] kv-pairs]
       (.add collector (Tuple. (into-array Object [k v])))))
   kv-tap)
 
-(defn connect
+(defn connect!
   "Connect the supplied source and sink with the supplied pipe."
-  [pipe source sink & {:keys [props] :or {props {}}}]
-  (doto (.connect (FlowConnector. props) source sink pipe)
+  [pipe source sink & {:keys [props]}]
+  (doto (.connect (flow-connector props) source sink pipe)
     (.complete)))
 
-(defn elephant->hfs
+(defn tuple-seq
+  "Returns all tuples in the supplied cascading tap as a Clojure
+  sequence."
+  [sink]
+  (with-open [it (.openForRead sink (job-conf))]
+    (doall (for [wrapper (iterator-seq it)]
+             (into [] (.getTuple wrapper))))))
+
+;; ## Transfer Functions
+
+(defn elephant->hfs!
   "Transfers all tuples from the supplied elephant-tap into the
   supplied cascading `sink`."
   [elephant-source sink]
-  (connect (Pipe. "pipe")
-           elephant-source
-           sink))
+  (connect! (Pipe. "pipe")
+            elephant-source
+            sink))
 
-(defn hfs->elephant
+(defn hfs->elephant!
   "Transfers all tuples from the supplied cascading `source` to the
   supplied elephant-tap."
   [source elephant-sink]
-  (connect (-> (Pipe. "pipe")
-               (KeyValTailAssembly. elephant-sink))
-           source
-           elephant-sink))
+  (connect! (-> (Pipe. "pipe")
+                (KeyValTailAssembly. elephant-sink))
+            source
+            elephant-sink))
 
 (defn fill-domain
   "Fills the supplied elephant-sink with the the supplied sequence of
@@ -111,38 +138,29 @@
   (test/with-fs-tmp [_ tmp]
     (-> (kv-tap tmp)
         (populate! pairs)
-        (hfs->elephant elephant-sink))))
+        (hfs->elephant! elephant-sink))))
 
-(defn get-tuples
-  "Returns all tuples in the supplied cascading tap as a Clojure
-  sequence."
-  [sink]
-  (with-open [it (.openForRead sink (jobconf))]
-    (doall (for [wrapper (iterator-seq it)]
-             (into [] (.getTuple wrapper))))))
+(defn read-etap-with-flow [tap]
+  (test/with-fs-tmp [fs tmp-path]
+    (let [sink (kv-tap tmp-path)]
+      (elephant->hfs! source sink)
+      (tuple-seq sink))))
+
+(defn just-tuples [actual]
+  (chatty-checker [expected]
+                  (= (set (tuple-seq actual))
+                     (set expected))))
 
 (tabular
  (fact "connect-test"
    (test/with-fs-tmp [_ src-tmp sink-tmp]     
      (let [sink (ElephantBaseTap. sink-tmp
-                                (DomainSpec. (JavaBerkDB.) (HashModScheme.) 4)
-                                (mk-options))]
+                                  (kv-spec 4)
+                                  (mk-options))]
        (fill-domain sink ?xs))))
  ?xs
  [[1 2] [3 4]]
  [["key" "val"] ["ham" "burger"]])
-
-(defn read-etap-with-flow [path]
-  (test/with-fs-tmp [fs tmp-path]
-    (let [source (ElephantBaseTap. path)
-          sink (kv-tap tmp-path)]
-      (elephant->hfs source sink)
-      (get-tuples sink))))
-
-(defn check-results
-  "TODO: Move over to edb proper."
-  [dpath pairs]
-  (fact (read-etap-with-flow dpath) => (just pairs :in-any-order)))
 
 ;; TODO: Invalid. Doesn't belong in this project; this makes far too
 ;; many assumptions about a thrift interface, etc. All we're concerned
@@ -163,9 +181,9 @@
           data2 {0 (barr 1)
                  10 (barr 100)}]
       (fill-domain sink data)
-      (check-results tmp data)
+      (tuple-seq sink) => (just (seq data) :in-any-order)
       (fill-domain sink data2)
-      (check-results tmp (conj data2 [(barr 1) nil])))))
+      (tuple-seq sink) => (just (conj (seq data2) [(barr 1) nil]) :in-any-order))))
 
 ;; TODO: Invalid. Doesn't belong in this project; this makes far too
 ;; many assumptions about a thrift interface, etc. All we're concerned
@@ -184,9 +202,9 @@
                  [(barr 2) (barr 2 2)]
                  [(barr 3) (barr 3)]]]
       (fill-domain sink data)
-      (check-results tmp data)
+      (tuple-seq sink) => (just (seq data) :in-any-order)
       (fill-domain sink data2)
-      (check-results tmp data3))))
+      (tuple-seq sink) => (just (seq data3) :in-any-order))))
 
 (fact "test-source"
   (test/with-fs-tmp [fs tmp]
@@ -209,17 +227,14 @@
           (t/kv-pairs= pairs (read-etap-with-flow dpath)) => true)))))
 
 ;; Example of how to do stuff now.
-(def spec
-  (DomainSpec. (JavaBerkDB.)
-               (HashModScheme.)
-               4))
 
 (defn populate [root idx]
-  (with-open [shard (.createShard spec root idx)]
+  (with-open [shard (.createShard (kv-spec 4) root idx)]
     (doseq [x (range 1000)]
       (.index shard (KeyValDocument. x 10)))))
 
 ;; or, you can create a domain store directly:
+
 (defn store []
   (test/with-fs-tmp [_ tmp]
     (DomainStore. tmp (DomainSpec. (JavaBerkDB.)
