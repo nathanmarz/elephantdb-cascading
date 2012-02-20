89e0c90d98399c5b6f1742758d071a878df0ed06
Sam Ritchie <sritchie09@gmail.com>
2012-02-19 16:51:44 -0800

    added support for travis.

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :dependencies [[elephantdb "0.2.0"]
                  [cascading/cascading-core "1.2.4"
                   :exclusions [org.codehaus.janino/janino]]]
-  :dev-dependencies [[org.clojure/clojure "1.3.0"]
+  :dev-dependencies [[org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                     [midje "1.3.1" :exclusions [org.clojure/clojure]]
+                     [org.clojure/clojure "1.2.1"]
                      [hadoop-util "0.2.7"]
-                     [jackknife "0.1.2"]
-                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
-                     [midje "1.3.0"]])
+                     [jackknife "0.1.2"]])
