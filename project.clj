df73b9263743b18b6780c424648ca196730bc843
Soren Macbeth <soren@dopeness.org>
2012-05-24 10:45:05 -0700

    exclude bundled hadoop jar in yieldbot edb jar

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :java-source-path "src/jvm"
   :javac-options {:debug "true" :fork "true"}
   :repositories {"conjars" "http://conjars.org/repo"}
-  :dependencies [[yieldbot/elephantdb "0.2.0-wip4"]
+  :dependencies [[yieldbot/elephantdb "0.2.0-wip4"
+                  :exclusions [org.apache.hadoop/hadoop-core]]
                  [org.slf4j/slf4j-api "1.6.1"]
                  [cascading/cascading-hadoop "2.0.0-wip-281"
                   :exclusions [org.codehaus.janino/janino
@@ -12,5 +13,4 @@
                      [org.clojure/clojure "1.3.0"]
                      [hadoop-util "0.2.8"]
                      [jackknife "0.1.2"]
-                     [org.apache.hadoop/hadoop-core "0.20.2-dev"]
                      [midje "1.3.0"]])
